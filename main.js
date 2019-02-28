4bf3466b80b48cb36095e76c0fc81816bd3fb2b0
eamedema <eamedema@gmail.com>
2019-02-27 22:43:54 -0500

    fixed radio button error

--- main.js
+++ main.js
@@ -2,22 +2,25 @@
 
 function addItem() {
 
-  let fruit = document.getElementById("fruitInput").value;
-  let veggie = document.getElementById("veggieInput").value;
+//define variables
+//by using .checked my objects are either true or false.
+//have to have the same input name to only select 1 of the 2
+  let fruit = document.getElementById("fruitInput").checked;
+  let veggie = document.getElementById("veggieInput").checked;
   let food = document.getElementById("name").value;
 
 
-  if (fruit === "fruit") {
-    let x = document.createElement("LI");
-    let t = document.createTextNode(`${food}`);
-    x.appendChild(t);
-    document.getElementById("fruit").appendChild(x);
-  } else {
-  let x = document.createElement("LI");
-  let t = document.createTextNode(`${food}`);
-  x.appendChild(t);
-  document.getElementById("veggies").appendChild(x);
-  }
 
+  if (fruit === true) {
+    let list = document.createElement("LI");
+    let typeOfFood = document.createTextNode(`${food}`);
+    list.appendChild(typeOfFood);
+    document.getElementById("fruit").appendChild(list);
+  } else if (veggie === true){
+    let list = document.createElement("LI");
+    let typeOfFood = document.createTextNode(`${food}`);
+    list.appendChild(typeOfFood);
+    document.getElementById("veggies").appendChild(list);
+  }
 
 }
